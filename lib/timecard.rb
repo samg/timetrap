16dcee3a410eb8badd3662cacb3fa5f1943c743a
Sam Goldstein <sgrock@gmail.com>
2009-03-31 00:41:09 -0700

    work on programming interface

---
+++ lib/timecard.rb
@@ -0,0 +1,150 @@
+require 'rubygems'
+require 'chronic'
+require 'sequel'
+# connect to database.  This will create one if it doesn't exist
+DB_NAME = defined?(TEST_MODE) ? nil : "#{ENV['HOME']}/.timecard.db"
+DB = Sequel.sqlite DB_NAME
+require File.join(File.dirname(__FILE__), 'timecard/models')
+require File.join(File.dirname(__FILE__), 'timecard/cli')
+
+module Timecard
+  extend self
+
+  def current_sheet= sheet
+    m = Meta.find_or_create(:key => 'current_sheet')
+    m.value = sheet
+    m.save
+  end
+
+  def current_sheet
+    unless Meta.find(:key => 'current_sheet')
+      Meta.create(:key => 'current_sheet', :value => 'default')
+    end
+    Meta.find(:key => 'current_sheet').value
+  end
+
+  def invoked_as_executable?
+    $0 == __FILE__
+  end
+
+  def entries sheet = nil
+    Entry.filter(:sheet => sheet)
+  end
+
+  def running?
+    !!active_entry
+  end
+
+  def active_entry
+    Entry.find(:sheet => Timecard.current_sheet, :end => nil)
+  end
+
+  def stop time = Time.now
+    while a = active_entry
+      a.end = time
+      a.save
+    end
+  end
+
+  def start note, time = Time.now
+    raise AlreadyRunning if running?
+    Entry.create :sheet => Timecard.current_sheet, :note => note, :start => time
+  rescue => e
+    say e.message
+  end
+
+  class AlreadyRunning < StandardError
+    def message
+      "Timecard is already running"
+    end
+  end
+
+  COMMANDS = {
+    "alter" => "alter the description of the active period",
+    "backend" => "open an the backend's interactive shell",
+    "display" => "display the current timesheet",
+    "format" => "export a sheet to csv format",
+    "in" => "start the timer for the current timesheet",
+    "kill" => "delete a timesheet",
+    "list" => "show the available timesheets",
+    "now" => "show the status of the current timesheet",
+    "out" => "stop the timer for the current timesheet",
+    "running" => "show all running timesheets",
+    "switch" => "switch to a new timesheet"
+  }
+
+  def invoke command, *args
+    invoke_command_if_valid(command, *args)
+  end
+
+  def invoke_command_if_valid command, *args
+    case (valid = COMMANDS.keys.select{|name| name =~ %r|^#{command}|}).size
+    when 0 then say "Invalid command: #{command}"
+    when 1 then send valid[0], *args
+    else; say "Ambigous command: #{command}"; end
+  end
+
+  def switch sheet = nil
+    if not sheet then say "No sheet specified"; return end
+    say "Switching to sheet " + Timecard.current_sheet = sheet
+  end
+
+  def list
+    say "Timesheets:"
+    sheets = Entry.map{|e|e.sheet} << Timecard.current_sheet
+    say(*sheets.uniq.sort.map do |str|
+      if str == Timecard.current_sheet
+        "  * %s" % str
+      else
+        "  - %s" % str
+      end
+    end)
+  end
+
+  def in *d
+    Timecard.start d.join(' ')
+  end
+
+  def out
+    Timecard.stop
+  end
+
+  def display
+    say DB[:entries].filter(:sheet => Timecard.current_sheet).all
+  end
+
+  def say *something
+    puts *something if Timecard.invoked_as_executable?
+  end
+
+  class Entry < Sequel::Model
+    def start= time
+      self[:start]= Chronic.parse(time) || time
+    end
+
+    def end= time
+      self[:end]= Chronic.parse(time) || time
+    end
+
+    # do a quick pseudo migration.  This should only get executed on the first run
+    set_schema do
+      primary_key :id
+      column :note, :string
+      column :start, :timestamp
+      column :end, :timestamp
+      column :sheet, :string
+    end
+    create_table unless table_exists?
+  end
+
+  class Meta < Sequel::Model(:meta)
+    set_schema do
+      primary_key :id
+      column :key, :string
+      column :value, :string
+    end
+    create_table unless table_exists?
+  end
+
+  invoke *ARGV if invoked_as_executable?
+end
--- spec/timecard_spec.rb
+++ spec/timecard_spec.rb
@@ -1,52 +1,130 @@
-require File.join(File.dirname(__FILE__), '..', 'timecard')
+TEST_MODE = true
+require File.join(File.dirname(__FILE__), '..', 'lib', 'timecard')
 require 'spec'
 
-describe Timecard::CLI do
-  it "should call a valid command" do
-    Timecard::CLI.should_receive(:alter).with('arg_1', 'arg_2')
-    Timecard::CLI.invoke 'alter', 'arg_1', 'arg_2'
+describe Timecard do
+  before :each do
+    Timecard::Entry.create_table!
+    Timecard::Meta.create_table!
   end
 
-  it "should call a valid command by an abbreviation" do
-    Timecard::CLI.should_receive(:alter).with('arg_1', 'arg_2')
-    Timecard::CLI.invoke 'a', 'arg_1', 'arg_2'
+  describe 'CLI' do
+    it "should call a valid command" do
+      Timecard.should_receive(:alter).with('arg_1', 'arg_2')
+      Timecard.invoke 'alter', 'arg_1', 'arg_2'
+    end
+
+    it "should call a valid command by an abbreviation" do
+      Timecard.should_receive(:alter).with('arg_1', 'arg_2')
+      Timecard.invoke 'a', 'arg_1', 'arg_2'
+    end
+
+    it "should not call an invalid command" do
+      Timecard.should_not_receive(:exec).with('arg_1', 'arg_2')
+      Timecard.invoke 'exec', 'arg_1', 'arg_2'
+    end
   end
 
-  it "should not call an invalid command" do
-    Timecard::CLI.should_not_receive(:exec).with('arg_1', 'arg_2')
-    Timecard::CLI.invoke 'exec', 'arg_1', 'arg_2'
+  describe "entries" do
+    it "should give the entires for a sheet" do
+      e = create_entry :sheet => 'sheet'
+      Timecard.entries('sheet').all.should include(e)
+    end
+
+    def create_entry atts = {}
+      Timecard::Entry.create({
+        :sheet => 's1',
+        :start => Time.now,
+        :end => Time.now,
+        :note => 'note'}.merge(atts))
+    end
+  end
+
+  describe "start" do
+    it "should start an new entry" do
+      @time = Time.now
+      Timecard.current_sheet = 'sheet1'
+      lambda do
+        Timecard.start 'some work', @time
+      end.should change(Timecard::Entry, :count).by(1)
+      Timecard::Entry.order(:id).last.sheet.should == 'sheet1'
+      Timecard::Entry.order(:id).last.note.should == 'some work'
+      Timecard::Entry.order(:id).last.start.to_i.should == @time.to_i
+      Timecard::Entry.order(:id).last.end.should be_nil
+    end
+
+    it "should be running if it is started" do
+      Timecard.should_not be_running
+      Timecard.start 'some work', @time
+      Timecard.should be_running
+    end
+
+    it "should raise and error if it is already running" do
+      lambda do
+        Timecard.start 'some work', @time
+        Timecard.start 'some work', @time
+      end.should change(Timecard::Entry, :count).by(1)
+    end
+  end
+
+  describe "stop" do
+    it "should stop a new entry" do
+      @time = Time.now
+      Timecard.start 'some work', @time
+      entry = Timecard.active_entry
+      entry.end.should be_nil
+      Timecard.stop @time
+      entry.refresh.end.to_i.should == @time.to_i
+    end
+
+    it "should not be running if it is stopped" do
+      Timecard.should_not be_running
+      Timecard.start 'some work', @time
+      Timecard.stop
+      Timecard.should_not be_running
+    end
+
+    it "should not stop it twice" do
+      Timecard.start 'some work'
+      e = Timecard.active_entry
+      Timecard.stop
+      time = e.refresh.end
+      Timecard.stop
+      time.to_i.should == e.refresh.end.to_i
+    end
+
   end
 
   describe 'switch' do
     it "should switch to a new sheet" do
-      Timecard::CLI.invoke 's', 'sheet1'
+      Timecard.invoke 's', 'sheet1'
       Timecard.current_sheet.should == 'sheet1'
-      Timecard::CLI.invoke 's', 'sheet2'
+      Timecard.invoke 's', 'sheet2'
       Timecard.current_sheet.should == 'sheet2'
     end
   end
 end
 
-describe Entry do
+describe Timecard::Entry do
   before do
     @time = Time.now
-    @entry = Entry.new
+    @entry = Timecard::Entry.new
   end
 
   describe 'attributes' do
-    it "should have a description" do
-      @entry.description = "world takeover"
-      @entry.description.should == "world takeover"
+    it "should have a note" do
+      @entry.note = "world takeover"
+      @entry.note.should == "world takeover"
     end
 
-    it "should have a start_time" do
-      @entry.start_time = @time
-      @entry.start_time.should == @time
+    it "should have a start" do
+      @entry.start = @time
+      @entry.start.should == @time
     end
 
-    it "should have a end_time" do
-      @entry.end_time = @time
-      @entry.end_time.should == @time
+    it "should have a end" do
+      @entry.end = @time
+      @entry.end.should == @time
     end
 
     it "should have a sheet" do
@@ -57,15 +135,15 @@ describe Entry do
 
   describe "parsing natural language times" do
     it "should set start time using english" do
-      @entry.start_time = "yesterday 10am"
-      @entry.start_time.should_not be_nil
-      @entry.start_time.should == Chronic.parse("yesterday 10am")
+      @entry.start = "yesterday 10am"
+      @entry.start.should_not be_nil
+      @entry.start.should == Chronic.parse("yesterday 10am")
     end
 
     it "should set end time using english" do
-      @entry.end_time = "tomorrow 1pm"
-      @entry.end_time.should_not be_nil
-      @entry.end_time.should == Chronic.parse("tomorrow 1pm")
+      @entry.end = "tomorrow 1pm"
+      @entry.end.should_not be_nil
+      @entry.end.should == Chronic.parse("tomorrow 1pm")
     end
   end
 end
