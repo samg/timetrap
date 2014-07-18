dc2af8c02373df250f4bd6888d2566f646a4482e
psyomn <lethaljellybean@gmail.com>
2014-07-18 01:56:08 -0400

    timetrap.rb: make directories on run if not exist

--- lib/timetrap.rb
+++ lib/timetrap.rb
@@ -13,8 +13,11 @@ require File.join(File.dirname(__FILE__), 'timetrap', 'cli')
 require File.join(File.dirname(__FILE__), 'timetrap', 'timer')
 require File.join(File.dirname(__FILE__), 'timetrap', 'formatters')
 require File.join(File.dirname(__FILE__), 'timetrap', 'auto_sheets')
+require File.join(File.dirname(__FILE__), 'timetrap', 'boot')
 module Timetrap
   DB_NAME = defined?(TEST_MODE) ? nil : Timetrap::Config['database_file']
+  # Make the .config/timetrap dir
+  Timetrap::Boot.new.run
   # connect to database.  This will create one if it doesn't exist
   DB = Sequel.sqlite DB_NAME
   CLI.args = Getopt::Declare.new(<<-EOF)
