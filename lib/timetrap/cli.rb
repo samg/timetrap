080ccb60d1052e378ab1650c9933cc7d2c8a9a65
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-05-31 19:56:36 +0200

    raise Timer::AlreadyRunning for CLI.resume

--- lib/timetrap/cli.rb
+++ lib/timetrap/cli.rb
@@ -289,6 +289,8 @@ COMMAND is one of:
     end
 
     def resume
+      raise Timer::AlreadyRunning if Timer.running?
+
       entry = case
               when args['-i']
                 entry = Entry[args['-i']]
