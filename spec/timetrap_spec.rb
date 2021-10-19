3024eef1e42e31f33f8a1344cbc38c8c81daf3f1
Francois Buys <buys.fran@gmail.com>
2021-10-19 21:04:42 +0200

    Extract bins specs to features directory

--- spec/timetrap_spec.rb
+++ spec/timetrap_spec.rb
@@ -1772,16 +1772,6 @@ END:VCALENDAR
     end
 
   end
-  describe 'bins' do
-    # https://github.com/samg/timetrap/pull/80
-    it 'should include a t bin and an equivalent timetrap bin' do
-      timetrap = File.open(File.expand_path(File.join(File.dirname(__FILE__), '..', 'bin', 'timetrap')))
-      t = File.open(File.expand_path(File.join(File.dirname(__FILE__), '..', 'bin', 't')))
-      expect(t.read).to eq timetrap.read
-      expect(t.stat.mode).to eq timetrap.stat.mode
-    end
-  end
-
 
   private
 
